40286ff8c210ee4912e288f6b4e7f771c7dc6d86
Anirudh <141948385+shoppluse@users.noreply.github.com>
2026-05-09 11:32:42 +0530

    Update format_converter.py

--- modules/format_converter.py
+++ modules/format_converter.py
@@ -4,6 +4,21 @@ def convert_format(input_path, output_path):
 
     clip = VideoFileClip(input_path)
 
-    clip.write_videofile(output_path)
+    # MP4 Conversion
+    if output_path.endswith(".mp4"):
+
+        clip.write_videofile(
+            output_path,
+            codec="libx264",
+            audio_codec="aac"
+        )
+
+    # AVI Conversion
+    elif output_path.endswith(".avi"):
+
+        clip.write_videofile(
+            output_path,
+            codec="png"
+        )
 
     clip.close()
